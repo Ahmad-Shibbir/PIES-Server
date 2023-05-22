5ad2eacb4df03727ef21cbb40474547a822c2131
Ahmad Shibbir <ahmadshibbir.iium@gmail.com>
2023-05-22 21:15:23 +0800

    connected with MongoDB

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log("hello!")
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("DB Connection Successfull!"))
+  .catch((err) => {
+    console.log(err);
+  });
+
+
+app.use(express.json());
+
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Backend server is running!");
+});
